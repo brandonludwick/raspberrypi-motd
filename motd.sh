cc01b6df55cb609d368c8ba73bd65d46fdfda4a8
brandonludwick <31369383+brandonludwick@users.noreply.github.com>
2020-09-09 06:10:53 -0700

    Update motd.sh

--- motd.sh
+++ motd.sh
@@ -130,6 +130,9 @@ label4="$borderBar  $(color $statsLabelColor "Home space....:") $label4$borderBa
 label5="$(extend "$(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")ºC")"
 label5="$borderBar  $(color $statsLabelColor "Temperature...:") $label5$borderBar"
 
+label6="$(extend "$(curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=0&locCode=NAM|US|CA|RIVERSIDE" | sed -n '/Currently:/ s/.*: \(.*\): \([0-9]*\)\([CF]\).*/\2°\3, \1/p')")"
+label6="$borderBar  $(color $statsLabelColor "Weather...:") $label5$borderBar"
+
 stats="$label1\n$label2\n$label3\n$label4\n$label5"
 
 # Print motd
